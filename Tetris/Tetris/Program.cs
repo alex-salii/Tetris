34d68daab7952558eb85208e11341ae27edd4974
alex-salii <69809960+alex-salii@users.noreply.github.com>
2022-01-08 13:11:14 +0200

    вывод двух символов

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -6,7 +6,34 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+
+            int x1 = 2;
+            int y1 = 3;
+            char c1 = '*';
+
+            Drow(x1, y1, c1);
+
+            int x2 = 4;
+            int y2 = 6;
+            char c2 = '#';
+
+            Drow(x2, y2, c2);
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+
+        static void Drow(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
         }
     }
 }
