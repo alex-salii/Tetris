1240ff90c74b4324dd9c7d592cece7bf701174d5
alex-salii <69809960+alex-salii@users.noreply.github.com>
2022-01-08 13:25:37 +0200

    Drow -> Draw

--- Tetris/Tetris/Program.cs
+++ Tetris/Tetris/Program.cs
@@ -14,13 +14,13 @@ namespace MyApp // Note: actual namespace depends on the project name.
             int y1 = 3;
             char c1 = '*';
 
-            Drow(x1, y1, c1);
+            Draw(x1, y1, c1);
 
             int x2 = 4;
             int y2 = 6;
             char c2 = '#';
 
-            Drow(x2, y2, c2);
+            Draw(x2, y2, c2);
 
 
 
@@ -30,7 +30,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         }
 
 
-        static void Drow(int x, int y, char c)
+        static void Draw(int x, int y, char c)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(c);
